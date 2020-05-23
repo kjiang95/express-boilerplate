2d708e886809b098d79ad2c074aae84f7f9c4a1d
Kevin Jiang <kjiang95@gmail.com>
2020-05-23 17:58:42 -0500

    fixed eslint issues

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-'use strict';
+
 
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const { NODE_ENV } = require('./config')
+const { NODE_ENV } = require('./config');
 
 const app = express();
 
@@ -22,14 +22,14 @@ app.get('/', (req, res) => {
 });
 
 app.use(function errorHandler(error, req, res, next) {
-  let response
+  let response;
   if (NODE_ENV === 'production') {
-   response = { error: { message: 'server error' } }
+    response = { error: { message: 'server error' } };
   } else {
-    console.error(error)
-    response = { message: error.message, error }
+    console.error(error);
+    response = { message: error.message, error };
   }
-  res.status(500).json(response)
-})
+  res.status(500).json(response);
+});
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,5 @@
-'use strict';
-
 const app = require('./app');
-const { PORT } = require('./config')
+const { PORT } = require('./config');
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
